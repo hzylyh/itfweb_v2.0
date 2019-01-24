fb7de774be9c75da781eb7092328a47e2fe3d7cc
John Holl <hzylyh@hotmail.com>
2019-01-24 17:40:41 +0800

    修改： 1、配置代理选项，解决跨域问题

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,16 @@
 // vue.config.js
 module.exports = {
     // 选项...
+  devServer: {
+    host: 'localhost',
+    port: 8081,
+    proxy: { // 配置跨域
+      '/v3': {
+        //要访问的跨域的api的域名
+        target: 'http://localhost:36000/',
+        ws: false,
+        changeOrigin: true,
+      }
+    },
+  }
 }
